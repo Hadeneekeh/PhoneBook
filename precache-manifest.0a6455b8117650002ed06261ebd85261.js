be6bdab30ce2621cbcdd016a31ef039e93b15fe9
Kafilat Abdulwahab <hadeneekeh01@gmail.com>
2019-06-26 16:54:08 +0100

    Updates

---
+++ precache-manifest.0a6455b8117650002ed06261ebd85261.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25c8c54f6728f6dcddaee28a7c544b22",
+    "url": "/PhoneBook/index.html"
+  },
+  {
+    "revision": "579fcdc76117c0856e96",
+    "url": "/PhoneBook/static/css/main.57d517f1.chunk.css"
+  },
+  {
+    "revision": "3a9e33913cab0720716d",
+    "url": "/PhoneBook/static/js/2.86a2265c.chunk.js"
+  },
+  {
+    "revision": "579fcdc76117c0856e96",
+    "url": "/PhoneBook/static/js/main.3781b2fc.chunk.js"
+  },
+  {
+    "revision": "eadacd478a93671e1e3d",
+    "url": "/PhoneBook/static/js/runtime~main.57ff495b.js"
+  }
+]);
